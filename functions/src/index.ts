1c471b47458d1f9bcd97e4a1db48164346e725b1
Pal Kerecsenyi <palkerecs@gmail.com>
2020-11-27 13:40:02 +0000

    Update QR code generator to work with website

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -27,27 +27,31 @@ export const getSignInToken = functions
         return await admin.auth().createCustomToken(userId);
     });
 
+interface QrCodeData {
+    password?: string;
+}
+
 export const getQrCode = functions
     .region('europe-west2')
-    .https.onRequest(async (req, res) => {
-        const password = req.query.password as string;
+    .https.onCall(async (data: QrCodeData) => {
+        const password = data.password as string;
 
         if (!password) {
-            res.status(400).send('Please provide the `password` parameter');
-            return;
+            return null;
         }
 
         const correctPassword = functions.config().mgsauth.sitepassword;
         if (password !== correctPassword) {
-            res.status(403).send('Incorrect password');
-            return;
+            return null;
         }
 
         const token = getCurrentToken();
         const [publicKey, iv] = await getPublicKey();
-
         const encryptedAes = encryptTotpKey(publicKey, iv, token);
 
-        const qrCode = await QRCode.toBuffer('mymgs-privkey-' + encryptedAes);
-        res.end(qrCode, 'binary');
+        const qrBuffer = await QRCode.toBuffer('mymgs-privkey-' + encryptedAes, {
+            width: 600,
+            margin: 0,
+        });
+        return qrBuffer.toString('base64');
     });
