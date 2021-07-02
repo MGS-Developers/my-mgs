2bc1371e5c456f6b5310f04e9f07ceb25673f445
Pal Kerecsenyi <palkerecs@gmail.com>
2021-07-02 16:09:25 +0100

    iOS build fixes

--- lib/widgets/sportsday/feed/media_tile.dart
+++ lib/widgets/sportsday/feed/media_tile.dart
@@ -4,7 +4,7 @@ import 'package:jiffy/jiffy.dart';
 import 'package:mymgs/data_classes/sportsday/feed_item.dart';
 import 'package:mymgs/helpers/responsive.dart';
 
-class MediaFeedTile extends StatefulWidget {
+class MediaFeedTile extends StatelessWidget {
   final MediaFeedItem media;
   final Timestamp timestamp;
 
@@ -13,98 +13,41 @@ class MediaFeedTile extends StatefulWidget {
     required this.timestamp,
   });
 
-  _MediaFeedTileState createState() => _MediaFeedTileState();
-}
-
-class _MediaFeedTileState extends State<MediaFeedTile> with SingleTickerProviderStateMixin {
-  late final AnimationController expandController;
-  late final Animation<double> animation;
-
-  bool expanded = false;
-
-  @override
-  void initState() {
-    super.initState();
-    expandController = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 300),
-      lowerBound: 0.4,
-    );
-    animation = CurvedAnimation(
-      parent: expandController,
-      curve: Curves.easeInOut,
-    );
-  }
-
-  @override
-  void dispose() {
-    expandController.dispose();
-    super.dispose();
-  }
-
-  void toggleExpansion() {
-    setState(() {
-      expanded = !expanded;
-    });
-    if (expanded) {
-      expandController.forward();
-    } else {
-      expandController.reverse();
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: toggleExpansion,
-      child: ShaderMask(
-        shaderCallback: (bounds) {
-          return LinearGradient(
-              begin: Alignment.bottomCenter,
-              end: Alignment.center,
-              colors: [Colors.transparent, Colors.white],
-          ).createShader(bounds);
-        },
-        blendMode: expanded ? BlendMode.dst : BlendMode.modulate,
-        child: SizeTransition(
-          axisAlignment: -1.0,
-          sizeFactor: animation,
-          child: Container(
-            padding: EdgeInsets.symmetric(
-              horizontal: Responsive(context).horizontalPadding,
-              vertical: 20,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: Responsive(context).horizontalPadding,
+        vertical: 20,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text.rich(
+            TextSpan(
               children: [
-                Text.rich(
-                  TextSpan(
-                    children: [
-                      TextSpan(
-                        text: "Update from "
-                      ),
-                      TextSpan(
-                        text: widget.media.author,
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      TextSpan(
-                        text: " — " + Jiffy(widget.timestamp.toDate()).fromNow(),
-                      )
-                    ],
-                    style: Theme.of(context).textTheme.bodyText1,
-                  ),
+                TextSpan(
+                    text: "Update from "
                 ),
-                const SizedBox(height: 10),
-                Text(
-                  widget.media.text,
-                  style: Theme.of(context).textTheme.bodyText2?.copyWith(
-                    fontSize: 16,
-                  ),
+                TextSpan(
+                  text: media.author,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                TextSpan(
+                  text: " — " + Jiffy(timestamp.toDate()).fromNow(),
                 )
               ],
+              style: Theme.of(context).textTheme.bodyText1,
             ),
           ),
-        ),
+          const SizedBox(height: 10),
+          Text(
+            media.text,
+            style: Theme.of(context).textTheme.bodyText2?.copyWith(
+              fontSize: 16,
+            ),
+          )
+        ],
       ),
     );
   }
